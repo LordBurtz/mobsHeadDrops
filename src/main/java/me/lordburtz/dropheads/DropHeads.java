83fbfedc75022fdfb6933e7ec6370b78d26bc0f8
LordBurtz <fridolin.karger@gmail.com>
2021-05-05 10:48:57 +0200

    initial commit

---
+++ src/main/java/me/lordburtz/dropheads/DropHeads.java
@@ -0,0 +1,17 @@
+package me.lordburtz.dropheads;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DropHeads extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
